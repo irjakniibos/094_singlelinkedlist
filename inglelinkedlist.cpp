c9607797469cf1910b3436b0a112e473f467c32a
irjakniibos <irzayaumilsyahrar55@gmail.com>
2024-04-26 14:24:50 +0700

    membuat algoritma untuk memasukkan node baru ke dalam daftar terurut berdasarkan NIM

--- inglelinkedlist.cpp
+++ inglelinkedlist.cpp
@@ -27,4 +27,18 @@ void addNode() {
             return;
         }
     }
+    Node* previous = START;
+    Node* current = STAR;
+    while ((current != NULL) && (nim >= current->noMhs))
+    {
+        if (nim == current->noMhs)
+        {
+            cout << "NIM sudah ada" << endl;
+            return;
+        }
+        previous = current;
+        current = current->next;
+    }
+    nodeBaru->next = current;
+    previous->next = nodeBaru;
 }
