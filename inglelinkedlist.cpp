35ec177aa3890fbfcea65540cc19d5fb377a2b11
irjakniibos <irzayaumilsyahrar55@gmail.com>
2024-04-26 15:00:27 +0700

    membuat switch case

--- inglelinkedlist.cpp
+++ inglelinkedlist.cpp
@@ -138,5 +138,22 @@ int main(){
 			cout << "5. Keluar" << endl;
 			cout << "Pilihan: ";
 			cin >> pilihan;
+            switch (pilihan)
+			{
+			case 1:
+				addNode();
+				cout << "Data Berhasil Ditambahkan" << endl;
+				system("pause");
+				system("cls");
+				break;
+			case 2:
+				if (listEmpty())
+				{
+					cout << "List Kosong" << endl;
+					system("pause");
+					system("cls");
+					break;
+				}
+            }
         }
     }
